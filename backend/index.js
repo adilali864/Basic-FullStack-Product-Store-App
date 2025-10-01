ac810feab251ff639f6767a83a5fbca5a51bbc4f
AdilKhan <asilali864@gmail.com>
2025-10-02 01:54:48 +0530

    ready for production

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,12 @@ const PORT = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 app.use(express.json());
+app.use("/api/products", productRoute);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get(/^\/.*$/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
@@ -25,5 +26,3 @@ app.listen(PORT, () => {
   connectDB();
   console.log(`Server Started at http://localhost:${PORT}`);
 });
-
-app.use("/api/products", productRoute);
